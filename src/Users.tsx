6cb34497129fb027f76b6cc31a935979f338160d
LuisaFC <luisabsb@gmail.com>
2024-11-07 21:52:03 -0300

    enable -> reativa usando state

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,31 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import { IUser } from "./types";
 import { sleep } from "./sleep";
-
+import { useState } from "react";
 
 export function Users() {
-  const { data, isLoading } = useQuery(
-    {
-      enabled: false,
-      queryKey: ["users"],
-      queryFn: async (): Promise<IUser[]> => {
-        await sleep()
-        const response = await fetch("http://localhost:3000/users");
-        return response.json();
-      },
-    }
-  );
+  const [signedIn, setSignedIn] = useState(false);
+  const { data, isLoading } = useQuery({
+    enabled: signedIn,
+    queryKey: ["users"],
+    queryFn: async (): Promise<IUser[]> => {
+      await sleep();
+      const response = await fetch("http://localhost:3000/users");
+      return response.json();
+    },
+  });
 
-  return(
+  return (
     <div>
-      {isLoading && 'Carregando...'}
+      <button
+        type="button"
+        className="bg-white text-black px-4 py-2 rounded-lg"
+        onClick={() => setSignedIn(true)}
+      >
+        Logar
+      </button>
 
-        {data?.map((user) => (
-          <div key={user.id}>
-            <strong className="block">{user.name}</strong>
-            <small>{user.email}</small>
-          </div>
-        ))}
-      </div>
-  )
+      {isLoading && "Carregando..."}
+
+      {data?.map((user) => (
+        <div key={user.id}>
+          <strong className="block">{user.name}</strong>
+          <small>{user.email}</small>
+        </div>
+      ))}
+    </div>
+  );
 }
